b18b4a0170fa8064d6410a909b018aedec9c20b7
azbayor2 <hanharry2@gmail.com>
2025-07-08 11:05:26 +0900

    added macro script for docker compose
    -> initialize certbot (get certificate)
    -> connect with nginx

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo docker-compose run --rm certbot certbot certonly --webroot --webroot-path=/var/www/certbot --email hanharry2@gmail.com --agree-tos --no-eff-email -d xn--hw4bo2pv3cz9f.com
+
+
+
+sudo docker-compose up -d --build
+
