79776c049929b1b1ef14f522d8ddc25ec11132ae
azbayor2 <hanharry2@gmail.com>
2025-07-21 21:23:19 +0900

    saved

--- Back/AI/run.py
+++ Back/AI/run.py
@@ -1,71 +1,12 @@
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
-from transformers import Trainer
-from transformers import TrainingArguments
-import numpy as np
-import evaluate
-import torch
 from flask import Flask, request, jsonify
-from urlextract import URLExtract
-#from pykospacing import Spacing
-import re
-
-model = AutoModelForSequenceClassification.from_pretrained("Kobert_Finetuned")
-print("모델 로딩 성공")
-tokenizer = AutoTokenizer.from_pretrained("monologg/kobert", trust_remote_code=True)
-print("토큰나이저 로딩 성공")
-extractor = URLExtract()
-#spacing = Spacing()
-
-# 전처리 함수
-def clean_text(text):
-    original = text
-
-    # 1. URL → [url]
-    urls = extractor.find_urls(original)
-    for url in urls:
-        original = original.replace(url, "[url]")
-
-    # 2. [url] → 임시 토큰으로 보호
-    original = original.replace("[url]", "__URL__")
-
-    # 3. 숫자 연속 → '*'
-    original = re.sub(r'\d+', '*', original)
-
-    # 4. 특수문자 제거 (한글, 영어, 숫자, *, 공백만 남기기)
-    original = re.sub(r'[^\w\s가-힣\*]', '', original)
-
-    # 5. 공백 압축
-    original = re.sub(r'\s+', ' ', original).strip()
-
-    # 6. 다시 [url] 복원
-    original = original.replace("__URL__", "[url]")
-
-    # 7. 띄어쓰기 교정
-    '''
-    try:
-        original = spacing(original)
-    except Exception as e:
-        print(f"PyKoSpacing error: {e}")
-    '''
-
-    return original
-
-print("평가모드")
-model.eval()
-
-def test_eval(input):
-    text = tokenizer(input, return_tensors='pt', padding=True, truncation=True)
-    with torch.no_grad():
-        ret = model(**text)
-        ret = ret.logits
-        return torch.argmax(ret, dim=-1).item()
-    
 
 app = Flask(__name__)
 
+app.add_url_rule('/text', view_func=, methods = ['POST'])
+app.add_url_rule('/image', view_func=, methods= ['POST'])
+
 @app.route('/text', methods=['POST'])
-def text_eval():
+def post_text():
     body = request.get_json()
     print(body['text'])
     cleaned = clean_text(body['text'])
