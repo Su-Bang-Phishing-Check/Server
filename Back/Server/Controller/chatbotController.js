cdc0d5ffac77a3b78d28bc7f1dac8dfcb971a388
azbayor2 <hanharry2@gmail.com>
2025-07-24 23:38:51 +0900

    implemented chatbot (needs critical + score and function)

--- Back/Server/Controller/chatbotController.js
+++ Back/Server/Controller/chatbotController.js
@@ -10,9 +10,10 @@ export async function chatbot(req, res)
         let new_data = {};
         new_data.temp = {};
         new_data.temp.next=[];
+        new_data.temp.next_2=[];
         new_data.options = [];
 
-        if(data.state==false)  //처음 시작
+        if(data.state==false)  //처음 시작 --> O
         {
             new_data.state=1;
             new_data.question = questions[0].question;
@@ -21,65 +22,143 @@ export async function chatbot(req, res)
                 new_data.options.push(option.label);
                 
             new_data.temp.cur_question = 0;
-            new_data.temp.has_followup = 0;
-            new_data.temp.is_on_followup = 0;
 
             return res.status(201).json(new_data);
         }
 
-        const prev_question = data.temp.cur_question;  //이전 문제 번호 가져오기
-        const selected = data.select;   //선택한 옵션 가져오기
-        let has_followup = data.temp.has_followup; //하위 질문이 있는지
-        let is_on_followup = data.temp.is_on_followup;  //현재 하위 질문에 있는지
-        let temp = data.temp.next;  //다음 질문 목록
+        let prev_question = data.temp.cur_question;
+        let followup = data.temp.followup;
+        let next = data.temp.next;
+        let selected = data.select;
+        let next_2 = data.temp.next_2;
 
-        //응답 처리 (구현중)
-
-        if(is_on_followup==0)   //상위 질문에서 하위 질문으로 넘어 갈 때 다음 문제 배열 초기화하기
+        new_data.state=true;
+    
+        if(prev_question==0)  //1번 문제에 대한 응답
         {
-            for(const sel_option of selected){
-                if(questions[prev_question].options[sel_option].has_followup==true)
-                    temp.push(sel_option)
-            }
-            is_on_followup=1;
-        }
-
-        if(is_on_followup && data.temp.length!=0)  //현재 하위 질문에 있고, 다음 하위 질문이 있을 때
-        {
-            const next_followup = temp.shift();
-            console.log(next_followup);
-            new_data.question = questions[prev_question].options[next_followup].label;
-            for(const option of questions[prev_question.options[next_followup].options])
+            //응답 처리 (추후 구현)
+            
+            new_data.question = questions[1].question; //2번 질문
+            for(const option of questions[1].options) //2번 질문 옵션
                 new_data.options.push(option.label);
-            new_data.temp.is_on_followup=1;
-            new_data.temp.has_followup=0;
-            new_data.temp.next = data.temp;
-            new_data.temp.cur_question = prev_question;
-        
+
+            new_data.temp.cur_question=1;
+            new_data.temp.followup = null;
+
             return res.status(201).json(new_data);
         }
 
-        //다음 상위 질문
-
-        const next_question = prev_question+1;
-        if(next_question>3) //질문이 끝났으면
+        if(prev_question==1)   //2번 질문에 대한 응답
         {
-            new_data.state=0;
-            new_data.message="설문 끝!";
-            res.status(201).json(new_data);
+            if(followup==null)   //상위 문제일 때  --> 하위 문제로 인계
+            {
+                for(const select of selected)
+                {
+                    if(questions[1].options[select].has_followup==1)
+                        next.push(select);
+                    else
+                        new_data.temp.next_2.push(questions[1].options[select].next_2)   //3번 질문을 위해 저장
+                }
+
+                console.log(next);
+            }
+
+            else  // 하위 문제일 때 --> 3번 질문을 위해 저장
+            {
+                for(const select of selected)
+                    next_2.push(questions[1].options[followup].followup.options[select].next_2);  //3번 질문을 위해 저장
+            }
+
+            if(next.length!=0)  //남아있는 하위 질문이 있으면
+            {
+                const cur_followup = next.shift();
+                console.log(questions[1].options[0]);
+
+                new_data.question = questions[1].options[cur_followup].followup.question;
+                for(const option of questions[1].options[cur_followup].followup.options)
+                    new_data.options.push(option.label);
+
+                new_data.temp.cur_question =1;
+                new_data.temp.followup = cur_followup;
+                new_data.temp.next = next;
+                new_data.temp.next_2 = next_2;
+
+                return res.status(201).json(new_data);
+            }
+
+            prev_question++;
         }
 
-        new_data.state=1;
-        new_data.question=questions[next_question].question;  //질문 넣기
-        for(const option of questions[next_question].options)  //옵션 넣기
-            new_data.options.push(option.label);
-        //new_data.temp = {};
-        new_data.temp.cur_question=next_question;  //현재 질문 번호 저장
-        //new_data.temp.next=[];
-        new_data.temp.has_followup= questions[next_question].options.has_followup;  //하위 질문 존재 여부 표기
-        new_data.temp.is_on_followup=false; //현재 하위 질문에 있는지
+        if(prev_question==2)
+        {
+            if(followup != null)  //첫 질문이 아닐 때
+            {
+                //선택 처리하기
+            }
 
-        return res.status(201).json({new_data});
+            if(next_2.length!=0) // 하위 질문이 있을 때
+            {
+                const cur_followup = next_2.shift();
+
+                new_data.question = questions[2].options[cur_followup].question;
+                for(const option of questions[2].options[cur_followup].options)
+                    new_data.options.push(option.label);
+
+                new_data.temp.cur_question = 2;
+                new_data.temp.followup = cur_followup;
+                new_data.temp.next_2 = next_2;
+
+                return res.status(201).json(new_data);
+            }
+
+            new_data.question = questions[3].question;
+            for(const option of questions[3].options)
+                new_data.options.push(option.label);
+            
+            new_data.temp.cur_question=3;
+            new_data.temp.followup=null;
+
+            return res.status(201).json(new_data);
+        }
+
+        if(prev_question==3)
+        {
+            if(followup==null) //상위 문제에 대한 응답이면
+            {
+                for(const select of selected)
+                {
+                    if(questions[3].options[select].has_followup==true)
+                        next.push(select);
+                    //나머지 응답 처리
+                }
+            }
+            else  //followup 문제의 응답이면 점수 처리
+            {
+
+            }
+
+            if(next.length!=0)
+            {
+                const cur_followup = next.shift();
+
+                new_data.question = questions[3].options[cur_followup].question;
+                for(const option of questions[3].options[cur_followup].options)
+                    new_data.options.push(option.label);
+
+                new_data.temp.cur_question = 3;
+                new_data.temp.followup = cur_followup;
+                new_data.temp.next = next;
+
+                return res.status(201).json(new_data);
+            }
+
+            prev_question++;
+        }
+
+        return res.status(201).json({
+            state: false,
+            message: "finished"
+        });
 
     } catch(err){
         console.log(err);
