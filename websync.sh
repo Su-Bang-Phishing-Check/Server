e99d691604f0b4315ee7ef3ab5c6d8abbe61feb8
azbayor2 <hanharry2@gmail.com>
2025-08-20 16:38:24 +0900

    fixed websync

--- websync.sh
+++ websync.sh
@@ -2,7 +2,7 @@ mkdir temp
 cd temp
 rm -rf ./*
 
-git clone https://github.com/Su-Bang-Phishing-Check/Front-Phishing-Check.git  #깃허브 주소
+git clone [깃허브 주소] #깃허브 주소
 cd Front-Phishing-Check
 git checkout main   ## 브랜치명
 
